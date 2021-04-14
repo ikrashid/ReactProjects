07d68e7fad62a3ed3068581dc027f175f2d520f1
Ikra Rashid <ikra.rashid252@gmail.com>
2021-04-14 11:30:11 -0400

    added a shop page that shows a preview of the collections. preview of collections utilizes our collectionItem component

--- e-commerce-project/src/App.js
+++ e-commerce-project/src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
-import MenuItem from './components/menuItem/MenuItem';
+import ShopPage from './pages/shop/shop';
 import HomePage from './pages/homepage/HomePage';
 import {Route, Switch, Link} from 'react-router-dom';
 
 function App() {
-  const HatsPage = () =>(
-    <div>
-      <h1>Hats!</h1>
-    </div>
-  )
-
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/shop/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
       
     </div>
---
+++ e-commerce-project/src/pages/shop/shop.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collectionPreview/CollectionPreview';
+
+class ShopPage extends Component {
+    constructor (props){
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+    render(){
+        const {collections} = this.state;
+        return(
+            <div>
+                {collections.map(({id, ...collectionProps}) => (<CollectionPreview key = {id} {...collectionProps}/>))}
+            </div>
+        )
+    }
+}
+
+export default ShopPage;

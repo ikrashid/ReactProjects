3527de069994f0ff1afdbe311f67e6644b30ba13
Ikra Rashid <ikra.rashid252@gmail.com>
2021-04-14 12:06:53 -0400

    created header component with logo and link to shop and temp link for contact

--- e-commerce-project/src/App.js
+++ e-commerce-project/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import ShopPage from './pages/shop/shop';
 import HomePage from './pages/homepage/HomePage';
 import {Route, Switch, Link} from 'react-router-dom';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>

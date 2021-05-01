a90a5187d77f22ba855a87d368085ae1980de4a6
Ikra Rashid <ikra.rashid252@gmail.com>
2021-05-01 11:08:15 -0400

    added cartIcon and cartDropdown components. also implemented cart toggling feature by utilizing Redux

---
+++ e-commerce-project/src/components/cartIcon/CartIcon.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './CartIcon.scss';
+import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
+import {connect} from 'react-redux';
+import toggleCartHidden from '../../redux/cart/cart-action';
+
+const CartIcon = ({toggleCartHidden}) =>(
+    <div className='cart-icon' onClick={toggleCartHidden}>
+        <ShoppingIcon className='shopping-icon'/>
+        <span className='item-count'>0</span>
+    </div>
+)
+
+const mapDispatchToProps = (dispatch) =>({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon);
+
+// click the icon, toggleCartHidden is dispatched since its mapped to the prop, this is an action in cart-action 
+// then the reducer is going to go through its switch cases and update the state as necessary
--- e-commerce-project/src/components/header/Header.js
+++ e-commerce-project/src/components/header/Header.js
@@ -4,8 +4,10 @@ import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
 import {connect} from 'react-redux';
+import CartIcon from '../cartIcon/CartIcon';
+import CartDropdown from '../cartDropdown/CartDropdown';
 
-const Header = ({signedIn}) =>(
+const Header = ({signedIn, hidden}) =>(
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -17,11 +19,18 @@ const Header = ({signedIn}) =>(
                 (<div className='option' onClick={() => auth.signOut()}>Sign Out</div>) 
                 : (<Link className='option' to='/signIn'></Link>)
             }
+            <CartIcon/>
         </div>
+        {hidden ? null : <CartDropdown/>}
     </div>
 
 );
 const mapStateToProps = state => ({
-    signedIn: state.user.currentUser
+    signedIn: state.user.currentUser,
+    hidden: state.cart.hidden
 })
 export default connect(mapStateToProps)(Header);
+
+// we can use hidden as a prop because we're getting access to the state (through the connect method)
+// why state.cart.hidden? because cart is the name of our reducer, and hidden is the property defined in cartReducer
+// then use this property to toggle the icon

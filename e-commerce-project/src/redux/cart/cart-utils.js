0c41b6995a3d4599431deb242edc32d1c51edecc
Ikra Rashid <ikra.rashid252@gmail.com>
2021-05-04 17:49:13 -0400

    checkout item component now can add and remove items

--- e-commerce-project/src/redux/cart/cart-utils.js
+++ e-commerce-project/src/redux/cart/cart-utils.js
@@ -7,3 +7,13 @@ export const addItemToCart = (cartItems, itemToAdd) => {
 
     return [...cartItems, {...itemToAdd, quantity: 1}]
 }
+
+export const removeItemFromCart = (cartItems, itemToRemove) => {
+    const existingItem = cartItems.find(item => item.id === itemToRemove.id);
+    if(existingItem.quantity == 1){
+        return cartItems.filter(item => item.id != itemToRemove.id)
+    }
+    else if(existingItem){
+        return cartItems.map(item => item.id == itemToRemove.id ? {...item, quantity: item.quantity-1} : item)
+    }
+}

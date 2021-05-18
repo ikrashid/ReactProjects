d06d671f232351c65109fc55afb473c7dd09eb12
Ikra Rashid <ikra.rashid252@gmail.com>
2021-05-18 13:37:16 -0400

    stripe integration and updated title + icon for app

--- e-commerce-project/src/pages/checkout/Checkout.js
+++ e-commerce-project/src/pages/checkout/Checkout.js
@@ -4,6 +4,7 @@ import CheckoutItem from '../../components/checkoutItem/CheckoutItem';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart-selectors';
+import StripeCheckoutButton from '../../components/stripeButton/StripeButton';
 const CheckoutPage = ({cartItems, totalPrice}) => (
     <div className='checkout-page'>
         <div className='checkout-header'>
@@ -26,9 +27,15 @@ const CheckoutPage = ({cartItems, totalPrice}) => (
         {
             cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>)
         }
-        <div className='total'>
-            <span>TOTAL: ${totalPrice}</span>
+        <div className='total'><span>TOTAL: ${totalPrice}</span></div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br/>
+            4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
         </div>
+        
+        <StripeCheckoutButton price={totalPrice} />
+        
     </div>
 )
 const mapStateToProps = createStructuredSelector({
